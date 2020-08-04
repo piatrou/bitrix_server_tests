29d4141001094663adc1f3fd5f34da9ce3d912f8
Alexandr Petrov <piatrou974@gmail.com>
2020-08-04 16:31:07 +0300

    feature(Disk): output format

--- bitrix_disk_test.php
+++ bitrix_disk_test.php
@@ -12,11 +12,34 @@ function getmicrotime()
 
                 for ($j = 0; $j < 4; $j++)
                 {
+
+                        $open_time = 0;
+                        $open_start = 0;
+                        $write_time = 0;
+                        $write_start = 0;
+                        $close_time = 0;
+                        $close_start = 0;
+                        $include_time = 0;
+                        $include_start = 0;
+                        $remove_time = 0;
+                        $remove_start = 0;
+
                         $s1 = getmicrotime();
                         for ($i = 0; $i < 100; $i++)
                         {
-                                $fn = str_replace("#i#", $i, $file_name);
+                            $open_start = getmicrotime();
+                            $write_start = getmicrotime();
+                            $close_start = getmicrotime();
+                            $include_start = getmicrotime();
+                            $remove_start = getmicrotime();
+                            $fn = str_replace("#i#", $i, $file_name);
+                            $open_time = getmicrotime() - $open_start;
+                            $write_time = getmicrotime() - $write_start;
+                            $close_time = getmicrotime() - $close_start;
+                            $include_time = getmicrotime() - $include_start;
+                            $remove_time = getmicrotime() - $remove_start;
                         }
+
                         $e1 = getmicrotime();
                         $N1 = $e1 - $s1;
 
@@ -24,19 +47,53 @@ function getmicrotime()
                         for ($i = 0; $i < 100; $i++)
                         {
                                 $fn = str_replace("#i#", $i, $file_name);
+
+                                $open_start = getmicrotime();
                                 $fh = fopen($fn, "wb");
+                                $open_time = getmicrotime() - $open_start;
+
+                                $write_start = getmicrotime();
                                 fwrite($fh, $content);
+                                $write_time = getmicrotime() - $write_start;
+
+                                $close_start = getmicrotime();
                                 fclose($fh);
+                                $close_time = getmicrotime() - $close_start;
+
+                                $include_start = getmicrotime();
                                 include($fn);
+                                $include_time = getmicrotime() - $include_start;
+
+                                $remove_start = getmicrotime();
                                 unlink($fn);
+                                $remove_time = getmicrotime() - $remove_start;
                         }
                         $e2 = getmicrotime();
                         $N2 = $e2 - $s2;
 
+                        $open[] = $open_time;
+                        $write[] = $write_time;
+                        $close[] = $close_time;
+                        $include[] = $include_time;
+                        $remove[] = $remove_time;
+
                         if ($N2 > $N1)
                                 $res[] = 100 / ($N2 - $N1);
                 }
+                
+                echo "\e[35m++++++++++++++++++++++++++ DISK TEST ++++++++++++++++++++++++++\e[0m\n";
+                echo "Result mark: ";
+                $result_mark = round( array_sum($res) / doubleval(count($res)) , 1);
 
-                echo array_sum($res)."\n";
-                echo doubleval(count($res))."\n";
-                echo array_sum($res) / doubleval(count($res))."\n";
+                if($result_mark > 10000){
+                    echo "\e[32m$result_mark\e[0m\n";
+                }else{
+                    echo "\e[31m$result_mark\e[0m, recommended more than 10000\n";
+                }
+                echo 'Open time: '.((array_sum($open) / doubleval(count($open)))*1000000)."\n";
+                echo 'Write time: '.((array_sum($write) / doubleval(count($write)))*1000000)."\n";
+                echo 'Close time: '.((array_sum($close) / doubleval(count($close)))*1000000)."\n";
+                echo 'Include time: '.((array_sum($include) / doubleval(count($include)))*1000000)."\n";
+                echo 'Remove time: '.((array_sum($remove) / doubleval(count($remove)))*1000000)."\n";
+                echo "\e[35m+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\e[0m\n";
+
